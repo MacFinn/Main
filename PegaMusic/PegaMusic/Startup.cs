f643d467ec41ff627ef9edd631ae994a006d15f4
MacFinn <finzygfx@gmail.com>
2015-02-11 11:43:20 +0000

    Created MVC project

---
+++ PegaMusic/PegaMusic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PegaMusic.Startup))]
+namespace PegaMusic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
